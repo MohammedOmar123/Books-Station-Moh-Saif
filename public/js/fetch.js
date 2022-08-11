a9345064a0888cd690c44be86a8eb16d8f0b1475
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-11 15:43:09 +0300

    I created fetch function to send get request

---
+++ public/js/fetch.js
@@ -0,0 +1,14 @@
+const fetch = (url, cb) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        cb(data);
+      }
+    }
+    xhr.open('GET', url, true);
+    xhr.send();
+  };
+};
+module.exports = fetch;
